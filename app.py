2c8cfeca2022135f3aa8adca71c3956c7caee487
Ribya Ann Thomas <ribyaannthomas@RibyasMA.local>
2022-12-13 15:12:42 +0530

    train booking updated

--- app.py
+++ app.py
@@ -2,12 +2,17 @@
 import os
 from flask import Flask, request, url_for, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
+
 cd = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
     os.path.join(cd, 'railway.sqlite3')
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.../railway.sqlite3'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.../railway.sqlite3'
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
 db = SQLAlchemy(app)
 db.init_app(app)
@@ -37,7 +42,7 @@ class Station(db.Model):
     TCarr = db.Column(db.Integer)  # boolean
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
@@ -45,6 +50,9 @@ class User(db.Model):
     gender = db.Column(db.String)
     age = db.Column(db.Integer)
     mob_no = db.Column(db.Integer)
+
+    def get_id(self):
+        return (self.user_id)
 
 
 class Ticket(db.Model):
@@ -69,17 +77,21 @@ class FoodOrder(db.Model):
     station_name = db.Column(db.String)  # foreign key
 
 
-global logged
-logged = False
-global cur_user
-cur_user = None
-# db.create_all()          need to call initially only once to create tables
+# db.create_all()  # need to call initially only once to create tables
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
 
 
 @app.route('/')
+@login_required
 def dashboard():
-    if not cur_user:
+    if not current_user:
         return render_template('landing.html')
+
     return render_template('index.html')
 
 
@@ -88,15 +100,11 @@ def login():
     if request.method == 'POST':
         email = request.form['email_id']
         password = request.form.get('pass')
-        print(password)
+        # print(password)
         user = db.session.query(User).filter(
             User.email == email, User.password == password).first()
         if user:
-            print(user)
-            global logged
-            logged = True
-            global cur_user
-            cur_user = user
+            login_user(user, remember=False)
             return redirect(url_for('dashboard'))
         else:
             flash('Wrong password or email id')
@@ -121,37 +129,46 @@ def register():
                             email=email, password=password)
             db.session.add(new_user)
             db.session.commit()
-            global cur_user
-            cur_user = new_user
-            print(cur_user)
+            login_user(user)
             return redirect(url_for('dashboard'))
     else:
         return render_template('register.html')
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    global cur_user
-    cur_user = None
+    logout_user()
     return redirect(url_for('dashboard'))
 
 
 @app.route('/book', methods=['POST', 'GET'])
 def book_tickets():
     if request.method == 'POST':
+        trains = ['TA', 'TB', 'TC']
+        avail_trains = []
         start = request.form['start']
         end = request.form['end']
-        # print(end)
-        # avail_trains = db.session.query(Train)#????
-        avail_trains = ['TA', 'TB', 'TC']  # temporarily
+
+        for train in trains:
+            Tavail = db.session.query(Station).all()
+            if train == 'TA':
+                if Tavail[int(start)-1].TAarr and Tavail[int(end)-1].TAarr:
+                    avail_trains.append('TA')
+            elif train == 'TB':
+                if Tavail[int(start)-1].TAarr and Tavail[int(end)-1].TBarr:
+                    avail_trains.append('TB')
+            else:
+                if Tavail[int(start)-1].TAarr and Tavail[int(end)-1].TCarr:
+                    avail_trains.append('TC')
+
+        # print(avail_trains)
         avail = []
         for train in avail_trains:
             savail = db.session.query(Train).filter(
-                Train.train_name == train).first().seats_no
-            # print(savail)
+                Train.train_name == train).first()
             # list of available seats in the required trains
             avail.append(savail)
-        # print(avail_trains)
         return render_template('train_list.html', trains=avail_trains, avail=avail)
     else:
         stations = db.session.query(Station).all()
@@ -159,26 +176,65 @@ def book_tickets():
 
 
 @app.route('/book/<string:train_name>', methods=['POST', 'GET'])
+def view_train(train_name):
+    if request.method == 'POST':
+        pass_no = request.form['passengers']
+        # print(pass_no)
+        seats = db.session.query(Seats).all()
+        avail_seats = []
+        if train_name == 'TA':
+            for seat in seats:
+                avail_seats.append(seat.availTA)
+        elif train_name == 'TB':
+            for seat in seats:
+                avail_seats.append(seat.availTB)
+        else:
+            for seat in seats:
+                avail_seats.append(seat.availTC)
+        return render_template('view_train.html', seats=avail_seats, train_name=train_name, pass_no=int(pass_no))
+    return 'view get'
+
+
+@app.route('/book/<string:train_name>/booking', methods=['POST'])
 def book_train(train_name):
     if request.method == 'POST':
         need = request.form.getlist('seat_book')
-        print(need)
-        # train = Train(train_name=train_name,seats_no=)
+        names = request.form.getlist('Name')
+        # print(names)
+        tot_avail_seats = db.session.query(Train).filter(
+            Train.train_name == train_name).first()
+        tot_avail_seats.seats_no = tot_avail_seats.seats_no - \
+            len(need)  # train booking
+        # print(tot_avail_seats.seats_no)
 
-        return 'hi'
-    seats = db.session.query(Seats).all()
-    avail_seats = []
-    if train_name == 'TA':
-        for seat in seats:
-            avail_seats.append(seat.availTA)
-    elif train_name == 'TB':
-        for seat in seats:
-            avail_seats.append(seat.availTB)
-    else:
-        for seat in seats:
-            avail_seats.append(seat.availTC)
+        for name in names:
+            ticket = Ticket(user_id=current_user.user_id,
+                            name=name)  # ticket booking
+            db.session.add(ticket)
+            # print(ticket)
+        seats = db.session.query(Seats).all()
+        if train_name == 'TA':
+            for seat in seats:
+                if str(seat.seat_no) in need:
+                    seat.availTA = 0
+        elif train_name == 'TB':
+            for seat in seats:
+                if str(seat.seat_no) in need:
+                    seat.availTB = 0
+        else:
+            for seat in seats:
+                if str(seat.seat_no) in need:
+                    seat.availTC = 0
+        db.session.commit()
+        return redirect(url_for('order_food'))
 
-    return render_template('view_train.html', seats=avail_seats, train_name=train_name)
+
+@app.route('/food', methods=['POST', 'GET'])
+def order_food():
+    if request.method == 'POST':
+        return render_template('food_booking.html')
+
+    return render_template('food_booking.html')
 
 
 @app.route('/cancel', methods=['POST', 'GET'])
@@ -191,9 +247,10 @@ def cancel_tickets():
 
 @app.route('/profile', methods=['GET'])
 def profile():
-    print(cur_user)
+    # print(cur_user)
     return render_template('profile.html')
 
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', debug=True, port=8080)
